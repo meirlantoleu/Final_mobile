11080fbccef39d899318be7f2dd459f8873cca03
meirlantoleu <meirlantoleu@gmail.com>
2021-05-27 23:24:45 +0600

    Final

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:quiz/quiz_page.dart';
+import 'quiz_brain.dart';
+
+QuizBrain quizBrain = new QuizBrain();
+
+void main() => runApp(Quizzler());
+
+class Quizzler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
